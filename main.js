27e044c45b807889021f2188fa21fc3854a5fcc9
Janka K <janakorycanova@gmail.com>
2022-02-26 21:35:31 +0100

    Zaklad panacka - panacek se nyni hybe po hracim poli pomoci sipek, je potreba osetrit pretekani do strany a smerem nahoru,kdy se ztraci z hraci plochy

--- main.js
+++ main.js
@@ -7,3 +7,65 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 
 
 // sem začni psát svůj program
+
+
+let xIndex = 50;
+let yIndex = 50;
+
+let sizeValuePct = "%";
+let sizeValuePx = "px";
+let stepValueRight = 100;
+let stepValueLeft = -100;
+
+let startPositionX= document.getElementById("panacek").style.left = String(xIndex) + sizeValuePct;
+let startpositionY = document.getElementById("panacek").style.top = String(yIndex) + sizeValuePct;
+
+
+let test = window.innerWidth;
+let test1 = window.innerHeight;
+
+console.log(test)
+console.log(test1)
+
+function getActualPosition(){
+	return document.getElementById("panacek").getBoundingClientRect();	
+}
+
+
+function move(e){
+	e = e || window.event;
+	if (e.keyCode === 37){
+		let makeStep = document.getElementById("panacek").style.left = getActualPosition().left + stepValueLeft + sizeValuePx;
+	}else if (e.keyCode === 39){
+		let makeStep = document.getElementById("panacek").style.left = getActualPosition().left + stepValueRight + sizeValuePx;
+	}else if (e.keyCode === 38){
+		let makeStep = document.getElementById("panacek").style.top = getActualPosition().top + stepValueLeft + sizeValuePx;
+	}else if (e.keyCode === 40){
+		let makeStep = document.getElementById("panacek").style.top = getActualPosition().top +stepValueRight + sizeValuePx;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
